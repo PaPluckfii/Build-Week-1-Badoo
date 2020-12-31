e3a3e7864722fc8d73c87a429d7e9831fe63a621
sanjoy <paulsanjoy9875@gmail.com>
2020-12-31 09:40:15 +0530

    additionalLayout+intent

---
+++ Badoo_Clone/app/src/main/java/com/example/badoo_clone/SignUp_DrinkSmoke.java
@@ -0,0 +1,14 @@
+package com.example.badoo_clone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignUp_DrinkSmoke extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up__drink_smoke);
+    }
+}
--- Badoo_Clone/app/src/main/java/com/example/badoo_clone/SignUp_Motive.java
+++ Badoo_Clone/app/src/main/java/com/example/badoo_clone/SignUp_Motive.java
@@ -2,13 +2,26 @@ package com.example.badoo_clone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class SignUp_Motive extends AppCompatActivity {
+
+    private TextView signUpMotive_mTvNext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up__motive);
+        signUpMotive_mTvNext=findViewById(R.id.signUpMotive_tvNext);
+        signUpMotive_mTvNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignUp_Motive.this,SignUp_SexualOrientation.class);
+                startActivity(intent);
+            }
+        });
     }
 }
