24e70a4022e39c76fa7635a49dae22dc7a588346
sanjoy <paulsanjoy9875@gmail.com>
2021-01-02 08:16:19 +0530

    ActivityPhoto

---
+++ Badoo_Clone/app/src/main/java/com/example/badoo_clone/SignUp_Photo.java
@@ -0,0 +1,28 @@
+package com.example.badoo_clone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SignUp_Photo extends AppCompatActivity {
+
+    private Button signUpPhoto_mBtnContinue;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up__photo);
+        signUpPhoto_mBtnContinue=findViewById(R.id.signUpPhoto_btnContinue);
+        signUpPhoto_mBtnContinue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignUp_Photo.this,SignUp_SetPassword.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+}
